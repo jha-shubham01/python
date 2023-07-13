fd0474b010774aec8b136a0ed4eff543e97cdd68
Shubham Jha <s.jha7777@gmail.com>
2023-07-13 12:02:35 +0530

    Add generators

---
+++ generators.py
@@ -0,0 +1,29 @@
+
+print("1. Check Even numbers")
+# 1. Check Even numbers
+def even_numbers():
+    num = 0
+    while True:
+        if num % 2 == 0:
+            yield num
+        num += 1
+
+
+even_nums = even_numbers()
+
+
+even_number_list = [next(even_nums) for _ in range(10)]
+print(even_number_list)
+
+
+print("2. Generate fibonacci series")
+def fibonacci():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+fibo = fibonacci()
+
+fibonacci_list = [next(fibo) for _ in range(10)]
+print(fibonacci_list)
