40b817d50d53b54a7d9719a12c8752f199b512fa
Shubham Jha <s.jha7777@gmail.com>
2024-01-12 13:16:57 +0530

    Add Comprehension in Python

--- loops.py
+++ loops.py
@@ -1,32 +1,49 @@
 
-# For Loop
-# Sequence can be list, tuple, set, dictionary, and string
+# # For Loop
+# # Sequence can be list, tuple, set, dictionary, and string
 
-sequence = ["apple", "banana", "orange"]
-for fruit in sequence:
-    # Code to be executed for each item
-    print(f"I love eating {fruit}!")
-
-
-# While Loop
-guess_count = 0
-while guess_count < 3:
-    guess = input("What is your guess? ")
-    guess_count += 1
-
-    if guess == "secret":
-        print("Congratulations! You guessed the secret!")
-        break
-
-if guess_count == 3:
-    print("Sorry, you ran out of guesses.")
+# sequence = ["apple", "banana", "orange"]
+# for fruit in sequence:
+#     # Code to be executed for each item
+#     print(f"I love eating {fruit}!")
 
 
-# For Else
-sequence = ["apple", "banana", "orange"]
-for fruit in sequence:
-    # Code to be executed for each item
-    print(f"I love eating {fruit}!")
-else:
-    # Code to be executed if the loop finishes normally
-    print("Else code block executed")
+# # While Loop
+# guess_count = 0
+# while guess_count < 3:
+#     guess = input("What is your guess? ")
+#     guess_count += 1
+
+#     if guess == "secret":
+#         print("Congratulations! You guessed the secret!")
+#         break
+
+# if guess_count == 3:
+#     print("Sorry, you ran out of guesses.")
+
+
+# # For Else
+# sequence = ["apple", "banana", "orange"]
+# for fruit in sequence:
+#     # Code to be executed for each item
+#     print(f"I love eating {fruit}!")
+# else:
+#     # Code to be executed if the loop finishes normally
+#     print("Else code block executed")
+
+
+# # Nested Loops
+# sequence = [["apple","banana", "orange"], ["Broccoli", "Garlic"]]
+# for items in sequence:
+#     print("Printing new items:")
+#     for item in items:
+#         print(f"Item is {item}!")
+
+# Comprehension For loop
+sequence = ["apple","banana", "orange"]
+# [print(fruit) for fruit in sequence]
+
+[print(fruit) for fruit in sequence if fruit.startswith("a")]
+
+[print(fruit) if fruit.startswith("a") else print(f'I don\'t like {fruit}!') for fruit in sequence]
+
